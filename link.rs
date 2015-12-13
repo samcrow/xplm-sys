715e4cbcf07b058998121d10d73039a2a500a46c
Sam Crow <samcrow@uw.edu>
2015-12-13 10:07:44 -0800

    Compile fixes for Linux

--- link.rs
+++ link.rs
@@ -7,11 +7,14 @@ fn main() {
     let target = var("TARGET").ok().expect("No TARGET environment variable");
 
     if target.contains("-apple-") {
-        println!("cargo:rustc-link-search=framework=../XPLM/SDK/Libraries/Mac/");
+        println!("cargo:rustc-link-search=framework=SDK/Libraries/Mac/");
         println!("cargo:rustc-link-lib=framework=XPLM");
         println!("cargo:rustc-link-lib=framework=XPWidgets");
     }
+    else if target.contains("-linux-") {
+        // Do nothing for Linux
+    }
     else {
-        panic!("Windows (target = {}) is not yet supported", target);
+        panic!("Unknown operating system (target = {}) is not supported", target);
     }
 }
