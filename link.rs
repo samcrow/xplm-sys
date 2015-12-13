662a096230aeda560e4cce6e8765c2f54642bbb3
Sam Crow <samcrow@uw.edu>
2015-12-13 11:26:10 -0800

    Link script fixes for Windows

--- link.rs
+++ link.rs
@@ -14,6 +14,17 @@ fn main() {
     else if target.contains("-linux-") {
         // Do nothing for Linux
     }
+    else if target.contains("-windows-") {
+        println!("cargo:rustc-link-search=SDK/Libraries/Win/");
+		if target.contains("x86_64") {
+			println!("cargo:rustc-link-lib=XPLM_64.lib");
+			println!("cargo:rustc-link-lib=XPWidgets_64.lib");
+		}
+		else {
+			println!("cargo:rustc-link-lib=XPLM.lib");
+			println!("cargo:rustc-link-lib=XPWidgets.lib");
+		}
+    }
     else {
         panic!("Unknown operating system (target = {}) is not supported", target);
     }
