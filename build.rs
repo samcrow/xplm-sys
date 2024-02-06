7a5f0ff65ea2fbec6cbb9496c2562594e3137113
Lucas Greuloch (greluc) <lucas.greuloch@pm.me>
2024-02-06 12:09:08 +0100

    try bindgen

--- build.rs
+++ build.rs
@@ -1,37 +1,21 @@
 use std::env;
 use std::path::PathBuf;
 
+extern crate bindgen;
+
 fn main() {
-    link_libraries();
-}
-
-/// On Mac OS and Windows targets, links the XPLM libraries
-fn link_libraries() {
-    // Get the absolute path to this crate, so that linking will work when done in another folder
     let crate_path = PathBuf::from(env::var_os("CARGO_MANIFEST_DIR").unwrap());
-    let target = env::var("TARGET").unwrap();
+    let library_path = crate_path.join("SDK\\Libraries\\Win");
+    println!("cargo:rustc-link-search={}", library_path.to_str().unwrap());
+    println!("cargo:rustc-link-lib=XPLM_64");
+    println!("cargo:rustc-link-lib=XPWidgets_64");
 
-    if target.contains("-apple-") {
-        let library_path = crate_path.join("SDK/Libraries/Mac");
-        println!(
-            "cargo:rustc-link-search=framework={}",
-            library_path.to_str().unwrap()
-        );
-        println!("cargo:rustc-link-lib=framework=XPLM");
-        println!("cargo:rustc-link-lib=framework=XPWidgets");
-    } else if target.contains("-linux-") {
-        // Do nothing for Linux
-    } else if target.contains("-windows-") {
-        let library_path = crate_path.join("SDK/Libraries/Win");
-        println!("cargo:rustc-link-search={}", library_path.to_str().unwrap());
-        if target.contains("x86_64") {
-            println!("cargo:rustc-link-lib=XPLM_64");
-            println!("cargo:rustc-link-lib=XPWidgets_64");
-        } else {
-            println!("cargo:rustc-link-lib=XPLM");
-            println!("cargo:rustc-link-lib=XPWidgets");
-        }
-    } else {
-        panic!("Target operating system not Mac OS, Linux, or Windows")
-    }
+    let bindings = bindgen::Builder::default()
+        .header("src/combined.h")
+        .generate()
+        .expect("Unable to generate bindings");
+
+    bindings
+        .write_to_file("bindgen.rs")
+        .expect("Couldn't write bindings!");
 }
