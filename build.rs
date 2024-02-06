abfaaa4942f4ff0a87d0758bb6e2bb6600850be6
Lucas Greuloch (greluc) <lucas.greuloch@pm.me>
2024-02-06 12:13:11 +0100

    revert back to multi os

--- build.rs
+++ build.rs
@@ -4,18 +4,59 @@ use std::path::PathBuf;
 extern crate bindgen;
 
 fn main() {
-    let crate_path = PathBuf::from(env::var_os("CARGO_MANIFEST_DIR").unwrap());
-    let library_path = crate_path.join("SDK\\Libraries\\Win");
-    println!("cargo:rustc-link-search={}", library_path.to_str().unwrap());
-    println!("cargo:rustc-link-lib=XPLM_64");
-    println!("cargo:rustc-link-lib=XPWidgets_64");
+    link_libraries();
+    use_bindgen();
+}
 
+/// On Mac OS and Windows targets, links the XPLM libraries
+fn link_libraries() {
+    // Get the absolute path to this crate, so that linking will work when done in another folder
+    let crate_path = PathBuf::from(env::var_os("CARGO_MANIFEST_DIR").unwrap());
+    let target = env::var("TARGET").unwrap();
+
+    if target.contains("-apple-") {
+        let library_path = crate_path.join("SDK/Libraries/Mac");
+        println!(
+            "cargo:rustc-link-search=framework={}",
+            library_path.to_str().unwrap()
+        );
+        println!("cargo:rustc-link-lib=framework=XPLM");
+        println!("cargo:rustc-link-lib=framework=XPWidgets");
+    } else if target.contains("-linux-") {
+        // Do nothing for Linux
+    } else if target.contains("-windows-") {
+        let library_path = crate_path.join("SDK/Libraries/Win");
+        println!("cargo:rustc-link-search={}", library_path.to_str().unwrap());
+        if target.contains("x86_64") {
+            println!("cargo:rustc-link-lib=XPLM_64");
+            println!("cargo:rustc-link-lib=XPWidgets_64");
+        } else {
+            println!("cargo:rustc-link-lib=XPLM");
+            println!("cargo:rustc-link-lib=XPWidgets");
+        }
+    } else {
+        panic!("Target operating system not Mac OS, Linux, or Windows")
+    }
+}
+
+fn use_bindgen() {
+    // The bindgen::Builder is the main entry point
+    // to bindgen, and lets you build up options for
+    // the resulting bindings.
     let bindings = bindgen::Builder::default()
+        // The input header we would like to generate
+        // bindings for.
         .header("src/combined.h")
+        // Tell cargo to invalidate the built crate whenever any of the
+        // included header files changed.
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks::new()))
+        // Finish the builder and generate the bindings.
         .generate()
+        // Unwrap the Result and panic on failure.
         .expect("Unable to generate bindings");
 
+    // Write the bindings to the $OUT_DIR/bindings.rs file.
     bindings
-        .write_to_file("bindgen.rs")
+        .write_to_file("src/bindgen.rs")
         .expect("Couldn't write bindings!");
 }
